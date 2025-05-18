3cbaa2dc3e3b219e6534a1e80fe4c0610388eef1
Eniya Madden <emadden4@luc.edu>
2025-05-17 21:56:44 -0500

    Update Property.java

--- src/main/java/com/landlord/Property.java
+++ src/main/java/com/landlord/Property.java
@@ -1,110 +1,44 @@
 package com.landlord;
 
 public class Property {
-    private String streetName;
-    private String city;
-    private String state;
-    private String zipCode;
-    private String propertyType;
-    private int bedrooms;
-    private int bathrooms;
-    private double monthlyRent;
     private String propertyId;
+    private String address;
+    private double rent;
+    private Tenant tenant;
 
-    // Constructor
-    public Property(String streetName, String city, String state, String zipCode, String propertyType,
-                    int bedrooms, int bathrooms, double monthlyRent, String propertyId) {
-        this.streetName = streetName;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.propertyType = propertyType;
-        this.bedrooms = bedrooms;
-        this.bathrooms = bathrooms;
-        this.monthlyRent = monthlyRent;
+    public Property(String propertyId, String address, double rent) {
         this.propertyId = propertyId;
-    }
-
-    // Getters and Setters
-    public String getStreetName() {
-        return streetName;
-    }
-
-    public void setStreetName(String streetName) {
-        this.streetName = streetName;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getPropertyType() {
-        return propertyType;
-    }
-
-    public void setPropertyType(String propertyType) {
-        this.propertyType = propertyType;
-    }
-
-    public int getBedrooms() {
-        return bedrooms;
-    }
-
-    public void setBedrooms(int bedrooms) {
-        this.bedrooms = bedrooms;
-    }
-
-    public int getBathrooms() {
-        return bathrooms;
-    }
-
-    public void setBathrooms(int bathrooms) {
-        this.bathrooms = bathrooms;
-    }
-
-    public double getMonthlyRent() {
-        return monthlyRent;
-    }
-
-    public void setMonthlyRent(double monthlyRent) {
-        this.monthlyRent = monthlyRent;
+        this.address = address;
+        this.rent = rent;
     }
 
     public String getPropertyId() {
         return propertyId;
     }
 
-    public void setPropertyId(String propertyId) {
-        this.propertyId = propertyId;
+    public String getAddress() {
+        return address;
     }
 
-    // Override toString() to return a formatted string
+    public double getRent() {
+        return rent;
+    }
+
+    public Tenant getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(Tenant tenant) {
+        this.tenant = tenant;
+    }
+
     @Override
     public String toString() {
         return "Property{" +
-                "streetName='" + streetName + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zipCode='" + zipCode + '\'' +
-                ", propertyType='" + propertyType + '\'' +
-
+                "PropertyID='" + propertyId + '\'' +
+                ", Address='" + address + '\'' +
+                ", Rent=$" + rent +
+                (tenant != null ? ", Tenant=" + tenant.getName() : ", Tenant=None") +
+                '}';
+    }
+}
