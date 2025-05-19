b5e5a78cea3f2fa5ba5a522d3b2b105b71f8e5f8
Eniya Madden <emadden4@luc.edu>
2025-05-18 21:24:43 -0500

    Landlord.java

--- src/main/java/com/landlord/Landlord.java
+++ src/main/java/com/landlord/Landlord.java
@@ -1,40 +1,23 @@
-package com.landlord;
+package com.example.madbuildllc;
+
 public class Landlord {
-    private String lnid;
+    private String id;
     private String name;
     private String email;
     private String phone;
 
-    public Landlord(String lnid, String name, String email, String phone) {
-        this.lnid = lnid;
+    public Landlord(String id, String name, String email, String phone) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
     }
 
-    public String getLnid() {
-        return lnid;
+    public String getId() {
+        return id;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    @Override
-    public String toString() {
-        return "Landlord{" +
-                "lnid='" + lnid + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                '}';
     }
 }
